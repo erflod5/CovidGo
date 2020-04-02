24ae0dee3e2cf916ce04a631029db57e4e4aaabc
Erik Flores <2883388711710@ingenieria.usac.edu.gt>
2020-04-02 15:15:36 -0600

    Rust Docker

--- Rust/target/debug/deps/hello_rocket-ee659988e57643e2.d
+++ Rust/target/debug/deps/hello_rocket-ee659988e57643e2.d
@@ -1,5 +1,5 @@
-/home/erflod5/projects/hello-rocket/target/debug/deps/hello_rocket-ee659988e57643e2: src/main.rs
+/home/erflod5/Escritorio/CovidGoServer/Rust/target/debug/deps/hello_rocket-ee659988e57643e2: src/main.rs
 
-/home/erflod5/projects/hello-rocket/target/debug/deps/hello_rocket-ee659988e57643e2.d: src/main.rs
+/home/erflod5/Escritorio/CovidGoServer/Rust/target/debug/deps/hello_rocket-ee659988e57643e2.d: src/main.rs
 
 src/main.rs:
